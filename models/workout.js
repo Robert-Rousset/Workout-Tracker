ba10f9a919e5dc8e4db9d0fe5e2926b09a6fec38
Robert Rousset <whyisrob@hotmail.com>
2021-07-10 14:10:33 +0800

    got seed working and added index to the models folder

--- models/workout.js
+++ models/workout.js
@@ -2,30 +2,38 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  type: {
-    type: String,
-    required: "Type is Required",
+  day: {
+    type: Date,
+    default: new Date(new Date().setDate(new Date().getDate())),
   },
-  name: {
-    type: String,
-    required: true,
-  },
-  weight: {
-    type: Number,
-  },
-  sets: {
-    type: Number,
-  },
-  reps: {
-    type: Number,
-  },
-  duration: {
-    type: Number,
-    required: true,
-  },
-  distance: {
-    type: Number,
-  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      weight: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      duration: {
+        type: Number,
+        required: true,
+      },
+      distance: {
+        type: Number,
+      },
+    },
+  ],
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
